0215e24cefbfe33a54bbfb89fb8ded8150fd326f
tomer <tomer@LAPTOP-9RP2E9JV>
2021-01-02 12:02:49 +0200

    show toolbar only when scrolling up

--- src/app/components/products-page/products-page.component.ts
+++ src/app/components/products-page/products-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-products-page',
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPageComponent implements OnInit {
 
+  currScrollPos = 0;
+
   constructor() { }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event: any) {
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + 
+      document.documentElement.offsetHeight;
+    const toolbar = window.document.getElementById('toolbar')!;
+    toolbar.style.zIndex = pos < this.currScrollPos ? '1000' : '0';
+    this.currScrollPos = pos;
+  }
 
   ngOnInit(): void {
   }
