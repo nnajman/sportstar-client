f90e097a8bd9b6ced383d9a5151eb3dc5db1ea4a
Noam Davidpur <noamdavidpur@gmail.com>
2021-01-07 19:54:01 +0200

    shopping cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductsPageComponent } from './components/products-page/products-page.component';
 import { ProductDetailsPageComponent } from './components/product-details-page/product-details-page.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'Men', component: ProductsPageComponent, data: {gender: 'Men'} },
   { path: 'Women', component: ProductsPageComponent, data: {gender: 'Women'} },
   { path: 'product-details', component: ProductDetailsPageComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
 ];
 
 @NgModule({
